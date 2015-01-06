8705f32b1438b198c1c03659731a47165b0d65e8
Steven Ng <steven.ng@temple.edu>
2015-01-06 15:45:11 -0500

    OaiRec specification test

--- spec/models/oai_rec_spec.rb
+++ spec/models/oai_rec_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe OaiRec, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'OaiRec Class' do
+    subject { OaiRec.new }
+
+    it { is_expected.to have_metadata_stream_of_type(OaiRecMetadata) }
+
+    it { is_expected.to respond_to(:creator) }
+	  it { is_expected.to respond_to(:title) }
+	  it { is_expected.to respond_to(:creator) }
+	  it { is_expected.to respond_to(:subject) }
+	  it { is_expected.to respond_to(:description) }
+	  it { is_expected.to respond_to(:publisher) }
+	  it { is_expected.to respond_to(:contributor) }
+	  it { is_expected.to respond_to(:date) }
+	  it { is_expected.to respond_to(:type) }
+	  it { is_expected.to respond_to(:format) }
+	  it { is_expected.to respond_to(:identifier) }
+	  it { is_expected.to respond_to(:source) }
+	  it { is_expected.to respond_to(:language) }
+	  it { is_expected.to respond_to(:relation) }
+	  it { is_expected.to respond_to(:coverage) }
+	  it { is_expected.to respond_to(:rights) }
+
+	  it { is_expected.to respond_to(:contributing_institution) }
+	  it { is_expected.to respond_to(:partner) }
+  end
 end

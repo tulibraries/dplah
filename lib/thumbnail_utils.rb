5894db59f399cd1750c0e48a3390e37d3d884dba
Katherine Lynch <katherine.lynch@temple.edu>
2015-02-26 12:39:36 -0500

    added common_repository_type to provider model and began sketching out thumbnailutils

--- lib/thumbnail_utils.rb
+++ lib/thumbnail_utils.rb
@@ -10,30 +10,16 @@ module ThumbnailUtils
   class CommonRepositories
   	class Contentdm
   		def self.asset_url
-  		  #http://contentdm1.accesspa.org/utils/getthumbnail/collection/churchslide/id/11
-  		  #churchslide = collection alias
-  		  # id/11 the "11" = CONTENTdm number/id (in local identifier)
+  		  
   		end
   	end
 
   	class Islandora
-  		#http://unbsj.cairnrepo.org/fedora/repository/unbsj:997/TN 
-  		#from
-  		#http://unbsj.cairnrepo.org/fedora/repository/unbsj:997
-
-  		#http://rudr.coalliance.org/fedora/repository/codr:2852/TN
-
-  		#AND
-
-  		#http://digitalcollections.detroitpubliclibrary.org/islandora/object/islandora%3A145930/datastream/TN/view
-  		#from 
-  		#http://digitalcollections.detroitpubliclibrary.org/islandora/object/islandora%3A145930
-
-
 
   	end
 
   	class Omeka
+
   	end
 
   	class Bepress

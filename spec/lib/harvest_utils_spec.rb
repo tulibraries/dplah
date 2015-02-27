cd4d7fd72c102f1508f20704991f82a156ab5aaf
Steven Ng <steven.ng@temple.edu>
2015-02-27 09:25:06 -0500

    Add convert specs

--- spec/lib/harvest_utils_spec.rb
+++ spec/lib/harvest_utils_spec.rb
@@ -3,8 +3,10 @@ require 'securerandom'
 
 RSpec.describe HarvestUtils do
   let (:config) { YAML.load_file(File.expand_path("#{Rails.root}/config/dpla.yml", __FILE__)) }
-  let (:pid_prefix) { config["pid_prefix"] }
-  let (:download_directory) { config["harvest_data_directory"] }
+  let (:pid_prefix) { config['pid_prefix'] }
+  let (:download_directory) { config['harvest_data_directory'] }
+  let (:convert_directory) { config['converted_foxml_directory'] }
+  let (:schema_url) { "http://www.fedora.info/definitions/1/0/foxml1-1.xsd" }
 
   context "Harvest Records" do
 
@@ -38,6 +40,39 @@ RSpec.describe HarvestUtils do
       expect(doc.xpath("//manifest/set_spec").first.text).to eq(p.set)
       expect(doc.xpath("//manifest/collection_name").first.text).to eq(p.collection_name)
       expect(doc.xpath("//manifest/contributing_institution").first.text).to eq(p.contributing_institution)
+    end
+  end
+
+  context "Convert Records" do
+
+    let(:p) { FactoryGirl.build(:provider_small_collection) }
+
+    before :each do
+      VCR.use_cassette "harvest_utils/provider_small_collection" do
+        HarvestUtils::create_log_file(p.name)
+        HarvestUtils::harvest(p)
+      end
+      VCR.use_cassette "harvest_utils/XML_schema" do
+        @xsd = Nokogiri::XML::Schema(open(schema_url))
+      end
+    end
+
+    after :each do
+      FileUtils.rm Dir.glob "#{convert_directory}/*.xml"
+    end
+
+    it "should convert a collection" do
+
+      HarvestUtils::convert(p)
+
+      file_count = Dir[File.join(convert_directory, '*.xml')].count { |file| File.file?(file) }
+      Dir.glob(File.join(convert_directory, '**', '*.xml')).each do |file|
+        doc = Nokogiri::XML(File.read(file))
+        @xsd.validate(doc).each do |error|
+          puts error
+        end
+        expect(@xsd.valid?(doc)).to be
+      end
     end
   end
 

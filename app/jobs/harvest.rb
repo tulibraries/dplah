03f93f1a5cb638f270875ede99ab8203fe4be2da
Katherine Lynch <katherine.lynch@temple.edu>
2015-03-17 15:32:49 -0400

    Have harvesting backgrounded

--- app/jobs/harvest.rb
+++ app/jobs/harvest.rb
@@ -1,7 +1,8 @@
 class Harvest
   @queue = :harvest
   def self.perform(provider)
-  	sleep(10)
-  	rec_count = HarvestUtils.harvest_action(provider)
+  	ruby_obj = JSON.parse(provider.to_json)
+    provider_obj = Provider.find(ruby_obj["id"])
+  	rec_count = HarvestUtils.harvest_action(provider_obj)
   end
 end

d2292aec62d1eac7bb586b5be8b6861f83d741c1
Katherine Lynch <katherine.lynch@temple.edu>
2015-03-27 12:22:08 -0400

    Moved identifier down in display, added View Object link under thumbnail (both per stakeholder request), neatened up footer displays, improved OAI Recs Helper

--- app/helpers/oai_recs_helper.rb
+++ app/helpers/oai_recs_helper.rb
@@ -1,7 +1,15 @@
 module OaiRecsHelper
 	def render_asset(document)
-          link_ident = document['identifier_tesim'].find { |id| id.start_with?("http") }
-          link_to(image_tag(document['thumbnail_tesim'].to_sentence, :alt => document['title_tesim'].to_sentence),link_ident) if document['thumbnail_tesim']
+		link_ident = asset_link(document)
+		asset_linked = link_to(image_tag(document['thumbnail_tesim'].to_sentence, :alt => document['title_tesim'].to_sentence),link_ident) if document['thumbnail_tesim']
+		obj = tag(:span, :class => 'glyphicon glyphicon-new-window')
+		return asset_linked
+	end
+	def asset_link(document)
+		link_ident = "#"
+		link_ident = document['identifier_tesim'].find { |id| id.start_with?("http") } if document['identifier_tesim']
+		return link_ident
 	end
 end
+
 

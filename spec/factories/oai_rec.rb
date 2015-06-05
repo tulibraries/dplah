fc666cbf390f24ce8f6ca81d3232eeedf3be10de
Steven Ng <steven.ng@temple.edu>
2015-06-05 16:24:18 -0400

    Create OAI rec controller specs

--- spec/factories/oai_rec.rb
+++ spec/factories/oai_rec.rb
@@ -2,22 +2,56 @@
 
 FactoryGirl.define do
   factory :oai_rec do
-    sequence(:title) { |n| "Record Title #{n}"}
-    creator "Record creator"
-    subject "Record subject"
-    description "Record description"
-    publisher "Record publisher"
-    contributor "Record contributor"
-    date "2014"
-    type "Rec%ype"
-    format "Record format"
-    sequence(:identifier) { |n| "recid:#{n}"}
-    source "Record source"
-    language "Record language"
-    relation "Record relation"
-    coverage "Record coverage"
-    rights "Record rights"
-    contributing_institution "Record contributing institution"
-    partner "Record partner"
+    sequence(:title) { |n| ["Record Title #{n}"]}
+    creator ["Record creator"]
+    subject ["Record subject"]
+    description ["Record description"]
+    publisher ["Record publisher"]
+    contributor ["Record contributor"]
+    date ["2014"]
+    type ["Rec%ype"]
+    format ["Record format"]
+    identifier ["Record identifier"]
+    source ["Record source"]
+    language ["Record language"]
+    relation ["Record relation"]
+    coverage ["Record coverage"]
+    rights ["Record rights"]
+	  common_repository_type ["Record common repository type"]
+	  endpoint_url ["http://example.com"]
+	  thumbnail ["http://example.com/thumbnail.jpg"]
+    contributing_institution ["Record contributing institution"]
+	  intermediate_provider ["Record intermediate provider"]
+	  collection_name ["Record collection name"]
+    partner ["Record partner"]
+	  set_spec ["Record set"]
+	  provider_id_prefix ["Record provider id prefix"]
+  end
+  
+  factory :oai_rec_invalid, class: OaiRec do
+    sequence(:title) { |n| ["Record Title #{n}"]}
+    creator ["Record creator"]
+    subject ["Record subject"]
+    description ["Record description"]
+    publisher ["Record publisher"]
+    contributor ["Record contributor"]
+    date ["2014"]
+    type ["Rec%ype"]
+    format ["Record format"]
+    identifier ["Record identifier"]
+    source ["Record source"]
+    language ["Record language"]
+    relation ["Record relation"]
+    coverage ["Record coverage"]
+    rights ["Record rights"]
+	  common_repository_type ["Record common repository type"]
+	  endpoint_url ["bad!//badurl"]
+	  thumbnail ["bad!!!//badurl/thumbnail.jpg"]
+    contributing_institution ["Record contributing institution"]
+	  intermediate_provider ["Record intermediate provider"]
+	  collection_name ["Record collection name"]
+    partner ["Record partner"]
+	  set_spec ["Record set"]
+	  provider_id_prefix ["Record provider id prefix"]
   end
 end

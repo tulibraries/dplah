3aa9344c2dfc85749033db1fe2d34362c7d5bfa2
Steven Ng <steven.ng@temple.edu>
2015-06-16 14:17:26 -0400

    Spec for define_thumbnail_common

--- spec/factories/oai_rec.rb
+++ spec/factories/oai_rec.rb
@@ -2,7 +2,7 @@
 
 FactoryGirl.define do
   factory :oai_rec do
-    sequence(:title) { |n| ["Record Title #{n}"]}
+    sequence(:title) { |n| "Record Title #{n}"}
     creator ["Record creator"]
     subject ["Record subject"]
     description ["Record description"]
@@ -29,7 +29,7 @@ FactoryGirl.define do
   end
   
   factory :oai_rec_invalid, class: OaiRec do
-    sequence(:title) { |n| ["Record Title #{n}"]}
+    sequence(:title) { |n| "Record Title #{n}"}
     creator ["Record creator"]
     subject ["Record subject"]
     description ["Record description"]
--- spec/lib/thumbnail_utils_spec.rb
+++ spec/lib/thumbnail_utils_spec.rb
@@ -4,6 +4,12 @@ RSpec.describe ThumbnailUtils do
 
   let (:config) { YAML.load_file(File.expand_path("#{Rails.root}/config/dpla.yml", __FILE__)) }
   let (:pid_prefix) { config['pid_prefix'] }
+  let (:contentdm_thumbnail_url) { "http://cdm16002.contentdm.oclc.org/utils/getthumbnail/collection/p16002coll9/id/1" }
+  let (:contentdm_pid) { "test-prefix:TEMPLE_p16002coll9_1" }
+  let (:bepress_thumbnail_url) { "http://example.com/example_collection/1234/thumbnail.jpg" }
+  let (:vudl_thumbnail_url) { "http://digital.library.example.com/files/vudl:1234/THUMBNAIL" }
+  let (:omeka_thumbnail_url) { "http://omeka.example.com/files/thumbnails/example_thumbnail.jpg" }
+
 
   before(:all) do
     OaiRec.destroy_all
@@ -15,26 +21,21 @@ RSpec.describe ThumbnailUtils do
 
   describe "Contentdm.asset_url" do
 
-    let (:pid) { "test-prefix:TEMPLE_p16002coll9_1" }
-    let (:thumbnail_url) { "http://cdm16002.contentdm.oclc.org/utils/getthumbnail/collection/p16002coll9/id/1" }
-
     subject {
       obj = FactoryGirl.create(:oai_rec)
       obj.endpoint_url = "http://cdm16002.contentdm.oclc.org/oai/oai.php"
       obj.thumbnail = "http://example.com/thumbnail.jpg"
       obj.set_spec = "p16002coll9"
-      oai_rec = OaiRec.new(pid: pid)
+      oai_rec = OaiRec.new(pid: contentdm_pid)
       oai_rec.endpoint_url = obj.endpoint_url
       oai_rec.set_spec =  obj.set_spec
       ThumbnailUtils::CommonRepositories::Contentdm.asset_url(oai_rec)
     }
 
-    it { is_expected.to match(thumbnail_url) }
+    it { is_expected.to match(contentdm_thumbnail_url) }
   end
   
   describe "Bepress.asset_url" do
-
-    let (:thumbnail_url) { "http://example.com/example_collection/1234/thumbnail.jpg" }
 
     subject {
       oai_rec = FactoryGirl.create(:oai_rec)
@@ -42,13 +43,11 @@ RSpec.describe ThumbnailUtils do
       ThumbnailUtils::CommonRepositories::Bepress.asset_url(oai_rec)
     }
 
-    it { is_expected.to match(thumbnail_url) }
+    it { is_expected.to match(bepress_thumbnail_url) }
 
   end
   
   describe "Vudl.asset_url" do
-
-    let (:thumbnail_url) { "http://digital.library.example.com/files/vudl:1234/THUMBNAIL" }
 
     subject {
       oai_rec = FactoryGirl.create(:oai_rec)
@@ -56,13 +55,11 @@ RSpec.describe ThumbnailUtils do
       ThumbnailUtils::CommonRepositories::Vudl.asset_url(oai_rec)
     }
 
-    it { is_expected.to match(thumbnail_url) }
+    it { is_expected.to match(vudl_thumbnail_url) }
 
   end
   
   describe "Omeka.asset_url" do
-
-    let (:thumbnail_url) { "http://omeka.example.com/files/thumbnails/example_thumbnail.jpg" }
 
     subject {
       oai_rec = FactoryGirl.create(:oai_rec)
@@ -70,22 +67,81 @@ RSpec.describe ThumbnailUtils do
       ThumbnailUtils::CommonRepositories::Omeka.asset_url(oai_rec)
     }
 
-    it { is_expected.to match(thumbnail_url) }
+    it { is_expected.to match(omeka_thumbnail_url) }
 
   end
 
   context "define_thumbnail_common" do
-    it "should render a valid CONTENTdm URL"
-    it "should render a valid Bepress thumbnail URL"
-    it "should render a valid VuDL thumbnail URL"
-    it "should render a valid Omeka thumbnail URL"
-  end
 
-  describe "define_thumbnail_pattern" do
+    before (:each) do
+      @provider = FactoryGirl.create(:provider) 
+    end
 
-  end
+    describe "CONTENTdm provider" do
 
-  describe "set_thumbnail" do
+      it "is a thumbnail URL" do
+        @provider.common_repository_type = "CONTENTdm"
+        obj = FactoryGirl.create(:oai_rec)
+        obj.endpoint_url = "http://cdm16002.contentdm.oclc.org/oai/oai.php"
+        obj.thumbnail = "http://example.com/thumbnail.jpg"
+        obj.set_spec = "p16002coll9"
+        oai_rec = OaiRec.new(pid: contentdm_pid)
+        oai_rec.endpoint_url = obj.endpoint_url
+        oai_rec.set_spec =  obj.set_spec
+        thumbnail_url = ThumbnailUtils.define_thumbnail_common(oai_rec, @provider)
+
+        expect(thumbnail_url).to match(contentdm_thumbnail_url)
+      end
+
+    end
     
+    describe "Bepress provider" do
+      it "is a thumbnail URL" do
+        @provider.common_repository_type = "Bepress"
+        oai_rec = FactoryGirl.create(:oai_rec)
+        oai_rec.description = ["http://example.com/example_collection/1234/thumbnail.jpg"]
+        thumbnail_url = ThumbnailUtils.define_thumbnail_common(oai_rec, @provider)
+
+        expect(thumbnail_url).to match(bepress_thumbnail_url)
+      end
+    end
+
+    describe "VuDL provider" do
+      it "is a thumbnail URL" do
+        @provider.common_repository_type = "VuDL"
+        oai_rec = FactoryGirl.create(:oai_rec)
+        oai_rec.identifier = ["http://digital.library.example.com/Record/vudl:1234"]
+        thumbnail_url = ThumbnailUtils.define_thumbnail_common(oai_rec, @provider)
+
+        expect(thumbnail_url).to match(vudl_thumbnail_url)
+      end
+    end
+
+    describe "Omeka provider" do
+      it "is a thumbnail URL" do
+        @provider.common_repository_type = "Omeka"
+        oai_rec = FactoryGirl.create(:oai_rec)
+        oai_rec.identifier = ["http://omeka.example.com/files/thumbnails/example_thumbnail.jpg"]
+        thumbnail_url = ThumbnailUtils.define_thumbnail_common(oai_rec, @provider)
+
+        expect(thumbnail_url).to match(omeka_thumbnail_url)
+      end
+    end
+
+    describe "Invalid provider" do
+      xit "is a thumbnail URL" do
+        common_repository_type = "Fake Repository"
+        @provider.common_repository_type = common_repository_type
+        oai_rec = FactoryGirl.create(:oai_rec)
+        expect(ThumbnailUtils.define_thumbnail_common(oai_rec, @provider)).to raise_error(SystemExit)
+      end
+
+    end
   end
+
+  it "defines a thumbnail pattern"
+
+  it "defines a thumbnail"
+
+  it "set a thumbnail"
 end

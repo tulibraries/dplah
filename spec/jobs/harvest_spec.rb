7df2916f4ac8be70ad340a4df0f9e8221e7c7e37
Steven Ng <steven.ng@temple.edu>
2015-06-17 13:17:16 -0400

    Spec harvest jobs

--- spec/jobs/harvest_spec.rb
+++ spec/jobs/harvest_spec.rb
@@ -1,16 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Harvest, type: :job do
-  before do
-    ResqueSpec.reset!
-  end
-
-  xit "adds Harvest.perform to the Harvest queue" do
+  it "adds Harvest.perform to the Harvest queue" do
     provider = FactoryGirl.create(:provider_small_collection).attributes
     provider_obj = Provider.find(provider["id"])
     allow(HarvestUtils).to receive(:harvest_action).with(provider_obj) {1}
-    allow(Resque).to receive(:enqueue).with(Harvest, provider)
-    Harvest.perform(provider)
-    expect(Harvest).to have_queue_size_of(1)
+    expect(Harvest.perform(provider)).to eq 1
   end
 end

a3f79335fc61b554aa2382fd6c2ed7f10cb97a87
Katherine Lynch <katherine.lynch@temple.edu>
2015-10-15 17:15:07 -0400

    Working but replacing every instance as opposed to last.  Must regex it.

--- app/models/oai_rec.rb
+++ app/models/oai_rec.rb
@@ -90,8 +90,7 @@ class OaiRec < ActiveFedora::Base
 		j = f.push("#{new_identifier}")
 		add_ident = "<dc:identifier>#{new_identifier}</dc:identifier>\n  "
 		self.update_attributes({"identifier" => j})
-		last_id = str.rindex("</dc:identifier>\n  ")
-		self.DC.content=self.DC.content.insert(last_id,"</dc:identifier>\n  <dc:identifier>#{add_ident}</dc:identifier>\n  ")
+		self.DC.content=self.DC.content.gsub("</dc:identifier>\n  ","</dc:identifier>\n  <dc:identifier>#{add_ident}</dc:identifier>\n  ")
 		self.save
 	end
 

85c3b111dfbb439c6a650a6224be06fe0a439729
Katherine Lynch <katherly@upenn.edu>
2016-03-23 12:56:36 -0400

    Switched to url_for, removed parameter

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -1,8 +1,7 @@
 module BlacklightHelper
 	 include Blacklight::BlacklightHelperBehavior
 	 def render_default_thumbnail_link(document, options = {})
-		 url = url_for_document(document)
-		 track_url = "#{polymorphic_url(url_for_document(document))}/track?counter=#{options[:counter]}"
+		 track_url = "#{url_for(url_for_document(document))}/track?counter=#{options[:counter]}&search_id=#{current_search_session.try(:id)}"
 		 default_thumb = link_to(image_tag("default-thumbnail.png", :alt => document['title_tesim'].to_sentence), url_for_document(document), :'data-context-href' => track_url)
 		 return default_thumb
 	 end

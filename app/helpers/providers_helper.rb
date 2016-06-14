eb63f4a0a43d805272ff96295f48d7109f6e4771
Katherine Lynch <katherly@upenn.edu>
2016-06-13 20:38:31 -0400

    Added disabling for intermediate provider buttons where no intermediate provider present

--- app/helpers/providers_helper.rb
+++ app/helpers/providers_helper.rb
@@ -4,4 +4,17 @@ module ProvidersHelper
       human_log_url = config['human_log_url']
       human_log_url
 	end
+
+	def render_harvest_all_intermediate_provider(provider)
+		if provider.intermediate_provider.present?
+			button = link_to 'Harvest all OAI Seeds from this Institution\'s Intermediate Provider', harvest_all_selective_intermediate_provider_provider_path(provider), method: :post, class: :triggerWaiting, data: { confirm: 'This may take a while for intermediate providers with institutions and associated OAI seeds. Are you sure?' }
+		else
+			button = content_tag(:span, "Harvest all OAI Seeds from this Institution's Intermediate Provider (not available)", class: "disabled-harvest-all-intermediate-provider")
+		end
+    return button
+	end
+
+	def oai_action_disabled_class(attribute_check)
+		return attribute_check.present? ? "oai-actions btn-provider" : "oai-actions"
+	end
 end

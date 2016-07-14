fbdfb1f72dd9379f0c69ed2a8ff2b7a3941a2342
Chad Nelson <chadbnelson@gmail.com>
2016-07-13 21:58:45 -0400

    Create script to handle setup

---
+++ setup.sh
@@ -0,0 +1,23 @@
+
+# Make sure external requirements are present
+command -v redis-server >/dev/null 2>&1 || { echo >&2 "Redis server should be installed  Aborting."; exit 1; }
+
+command -v xsltproc >/dev/null 2>&1 || { echo >&2 "xsltproc should be available at the command line, but it's not installed.  Aborting."; exit 1; }
+
+# copy config files into place
+
+cp config/dpla.yml.example config/dpla.yml
+
+bundle install
+
+bundle exec rake db:migrate
+bundle exec rails g hydra:jetty
+bundle exec rake jetty:config
+
+bundle exec rake jetty:start
+
+sleep 10
+
+rails server -d 
+
+resque-pool --daemon 

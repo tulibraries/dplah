791a476d52e2d59e3ec1bd4b53e25e925d6e3cc6
Chad Nelson <chadbnelson@gmail.com>
2016-08-04 10:39:32 -0400

    Jobs do not fail if no email is specified in seed. (#115)
    Related to #85.

--- app/mailers/harvest_mailer.rb
+++ app/mailers/harvest_mailer.rb
@@ -3,30 +3,36 @@ class HarvestMailer < ActionMailer::Base
   default from: config['email_sender'],
           to: config['email_recipient']
 
+  def define_headers(subject_text, provider=nil)
+    headers = {subject: subject_text}
+    headers.merge({to: provider.email}) if provider && provider.email
+    headers
+  end
+
   def harvest_complete_email(provider, logfile)
-    @provider = provider
     attachments[File.basename(logfile)] = File.read(logfile)
     subject_text = I18n.t('dpla.harvest_mailer.harvest_subject')
-    m = mail(:to=> provider.email, :subject => subject_text)
+    headers = define_headers(subject_text, provider)
+    mail(headers)
   end
 
   def dumped_whole_index_email(logfile)
     attachments[File.basename(logfile)] = File.read(logfile)
     subject_text = I18n.t('dpla.harvest_mailer.dump_whole_index_subject')
-    m = mail(subject: subject_text)
+    mail(subject: subject_text)
   end
 
   def dump_and_reindex_by_institution_email(provider, logfile)
-    @provider = provider
     attachments[File.basename(logfile)] = File.read(logfile)
     subject_text = I18n.t('dpla.harvest_mailer.dump_and_reindex_subject')
-    m = mail(to: provider.email, subject: subject_text)
+    headers = define_headers(subject_text, provider)
+    mail(headers)
   end
 
   def dump_and_reindex_by_collection_email(provider, logfile)
-    @provider = provider
     attachments[File.basename(logfile)] = File.read(logfile)
     subject_text = I18n.t('dpla.harvest_mailer.dump_and_reindex_subject')
-    m = mail(to: provider.email, subject: subject_text)
+    headers = define_headers(subject_text, provider)
+    mail(headers)
   end
 end

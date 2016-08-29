f374b4a697581214ec1a2084d640fad2b2605b28
Chad Nelson <chadbnelson@gmail.com>
2016-08-29 16:06:21 -0400

    Use devise differently (#131)
    * Don't allow open registration
    
    * Clean up indentation poblems

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,11 @@ Rails.application.routes.draw do
   get 'sponsors' => 'high_voltage/pages#show', id: 'sponsors'
 
   blacklight_for :catalog
-  devise_for :users
+  devise_for :users, :skip => [:registrations] 
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
 
   authenticate :user do
     mount Resque::Server.new, at: "/resque"

77634bf936bdf7986cfd6eaca40b13bfa27626db
Chad Nelson <chad.nelson@temple.edu>
2016-09-15 16:22:05 -0400

    Make facets configurable

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,12 @@ module DplaBl
 
     config.autoload_paths += %W(#{config.root}/lib)
 
+
+    Rails.application.configure do
+      config.enabled_facets = config_for(:facets).with_indifferent_access
+    end
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

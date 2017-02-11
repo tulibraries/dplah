997f646fcae40df27061473bf69d20090bfe2ae9
Katherine Lynch <katherly@upenn.edu>
2017-02-11 15:25:19 -0500

    Bepress identifier fixes

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -2,7 +2,7 @@ module BlacklightHelper
 	 include Blacklight::BlacklightHelperBehavior
 	 def render_default_thumbnail_link(document, options = {})
 		 track_url = "#{url_for(url_for_document(document))}/track?counter=#{options[:counter]}&search_id=#{current_search_session.try(:id)}"
-		 default_thumb = link_to(image_tag("default-thumbnail.png", :alt => document['title_tesim'].to_sentence), url_for_document(document), :'data-context-href' => track_url)
+		 default_thumb = document['title_tesim'].present? ? link_to(image_tag("default-thumbnail.png", :alt => document['title_tesim'].to_sentence), url_for_document(document), :'data-context-href' => track_url) : nil
 		 return default_thumb
 	 end
 end

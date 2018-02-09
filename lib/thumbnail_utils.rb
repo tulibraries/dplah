5b705e13cc148db0ecc998d86015a8ae534b64c7
Steven Ng <steven@nomadicoder.com>
2018-02-09 16:24:18 -0500

    Generic regular expression

--- lib/thumbnail_utils.rb
+++ lib/thumbnail_utils.rb
@@ -68,7 +68,7 @@ module ThumbnailUtils
         asset_url = ''
         url = URI.parse(obj.endpoint_url)
         obj.identifier.select{|id| !id.include?(' ')}.each do |ident|
-          ident = /[[:alnum:]]:PRESBY_(.*)/.match(obj.pid)[1].gsub("_",":") if obj.provider_id_prefix == "PRESBY"
+          ident = /[[:alnum:]]:#{obj.provider_id_prefix}_(.*)/.match(obj.pid)[1].gsub("_",":") if obj.provider_id_prefix == "PRESBY"
           Rails.logger.info "ISLANDORA_IDENTIFIER IS #{ident}"
           asset_url = "#{url.scheme}://#{url.host}/islandora/object/#{ident}/datastream/TN/view/"
         end

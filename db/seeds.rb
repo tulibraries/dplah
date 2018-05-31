1647b45f27bb25ad1f92c137f68df923a6f06f0b
Chad Nelson <chad.nelson@temple.edu>
2018-05-31 13:14:50 -0400

    Don't include seeds, actually

--- db/seeds.rb
+++
@@ -1,6 +0,0 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-User.create!({ email: "admin@example.org", password: "password", password_confirmation: "password" })
